b51f72876f300a5da072b8d78134c62c59f60e03
dexty1 <77596597+dexty1@users.noreply.github.com>
2025-01-14 16:58:52 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+ui_page 'html/ui.html'
+lua54 'yes'
+
+client_script 'dexty.lua'
+server_script 'server.lua'
+shared_scripts {'@ox_lib/init.lua'}
+
+files {
+	'html/ui.html',
+	'html/main.js',
+	'html/style.css',
+}
+
